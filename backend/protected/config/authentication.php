9d3f7f2f7c8a9fa59fd45e1647e31d07df875643
anderson <anderson.a.muller@gmail.com>
2012-10-25 01:37:48 +0200

    dummy

--- backend/protected/config/authentication.php
+++ backend/protected/config/authentication.php
@@ -1,9 +1,9 @@
 <?php
 return array(
 	'class'           => 'HttpDigestAuthentication',
-	'opaque'          => 'nvvzxfh9wqery91742',
+	'opaque'          => 'nvvzx9wqery91742',
 	'realm'           => 'Yii REST Sample Backend',
-	'privateKey'      => '78501oghaoasdfoar7128571',
+	'privateKey'      => '78501aoasdfoar7128571',
 	'nonceLife'       => 300,
 	'countDifference' => 5,
 	'passwordHashed'  => true,
